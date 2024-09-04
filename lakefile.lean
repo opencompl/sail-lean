8042429f62c532aa803cba93797388221cd304e3
Jakob von Raumer <jakob@von-raumer.de>
2024-09-04 13:47:58 +0100

    add quote4 dependency

--- lakefile.lean
+++ lakefile.lean
@@ -4,6 +4,9 @@ open Lake DSL
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4" @ "master"
 
+require Qq from git
+  "https://github.com/leanprover-community/quote4" @ "master"
+
 package «SailLean» {
   -- add package configuration options here
 }
